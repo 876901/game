779aedf2a7cc032d0e32585d569b79e1d5d7f3cd
Coseung <149292617+Coseung@users.noreply.github.com>
2024-05-14 19:33:32 +0900

    도로
    도로추가

--- Assets/Hpbar.cs
+++ Assets/Hpbar.cs
@@ -5,14 +5,16 @@ public class Hpbar : MonoBehaviour
 {
     public Slider slider; // 슬라이더 UI 요소에 대한 참조
     public static float maxHP;
+    public static float HP;
 
-    public void SetMaxHP(int maxHP)
+
+    public void SetMaxHP(float maxHP)
     {
         slider.maxValue = maxHP; // 슬라이더의 최대 값 설정
-        slider.value = maxHP; // 초기에 현재 값을 최대 값으로 설정
+        slider.value = HP; // 초기에 현재 값을 최대 값으로 설정
     }
 
-    public void SetHP(int hp)
+    public void SetHP(float hp)
     {
         slider.value = hp; // 현재 HP를 반영하기 위해 슬라이더 값 업데이트
     }
